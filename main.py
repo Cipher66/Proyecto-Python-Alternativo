c713a4f936aed42ca101ecde98cbbffbdc961dc2
Sergio Chavez <Sergio Chavez>
2017-05-27 00:43:20 +0200

    V0.1
    >Subido el archivo "main"

---
+++ main.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+print("Bienvenido a la aplicacion de ArmA III")
+print("¿Que quieres hacer?")
+print("1. Ver vehículos")
+print("2. Ver personajes")
+print("3. Ver DLC's")
+opcion = raw_input("Escribe tu opción: ")
